c1a388983b5cd399bb8b849be8374a7eb7140ab2
0007Ganesh <jagdale0007@gmail.com>
2025-09-25 10:51:21 +0530

    Create water_pump.c

---
+++ water_pump.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int water_level;
+    printf("Enter water level (between 0-5 or 495-500):");
+    scanf("%d", &water_level);
+    
+    printf("You have entered %d water level\n", water_level);
+
+    if ( water_level <= 5) {
+        printf("Motor ON\n");
+    } else if (water_level >= 495) {
+        printf("Motor OFF\n");
+    } else {
+        printf("Motor OFF\n");
+    }
+
+}
